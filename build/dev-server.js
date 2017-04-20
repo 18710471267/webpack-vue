5fc46ce20878ee2df93447ea2d94244e6a88551f
张娇 <397165397@qq.com>
2017-04-20 17:29:01 +0800

    通过webpack-dev-middleware关联webpack和express，将index.html放在9090端口下

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,7 +1,24 @@
+var webpack = require('webpack') 
 var express = require('express')
 var opn = require('opn')
 var app = express()
 var port=9090
+var htmlWebpackPlugin = require('html-webpack-plugin')
+var compiler = webpack({
+    entry:'./src/entry.js',
+    output:{
+        path:"/",
+        filename:"dist/bundle.js"
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            filename:"index.html",
+            template:"index.html"
+        })
+    ]
+})
+var middleWare = require('webpack-dev-middleware')(compiler,{})
+app.use(middleWare)
 app.listen(port,function(){
     opn("http://localhost:"+port)
 })
