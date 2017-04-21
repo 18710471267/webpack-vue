1cb63276ec22a1b8336ff745146bc221240c4e4b
张娇 <397165397@qq.com>
2017-04-21 15:57:32 +0800

    设置生产环境每次编译之前先删除老的编译数据

--- build/build.js
+++ build/build.js
@@ -3,6 +3,7 @@ var compiler = require('./webpack.prod.conf')
 
 require('shelljs/global')
 var assetsPath = './dist/static/'
+rm('-rf','./dist')
 mkdir('-p',assetsPath)
 cp('-R','static/*',assetsPath)
 
