859322e96b2223011634b359e1faf715a17eebe8
张娇 <397165397@qq.com>
2017-04-21 15:51:59 +0800

    设置在生产环境对静态文件的处理

--- build/build.js
+++ build/build.js
@@ -1,5 +1,11 @@
 var webpack = require('webpack')
 var compiler = require('./webpack.prod.conf')
+
+require('shelljs/global')
+var assetsPath = './dist/static/'
+mkdir('-p',assetsPath)
+cp('-R','static/*',assetsPath)
+
 webpack(compiler,function (err,stats) {
     
 })
